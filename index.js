6e6afe7e1787f036792e7dc186b3afc0fc99d5af
SaiSuprith Jukanti <jukantisuprith@gmail.com>
2025-02-16 17:33:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
 
 const allowedOrigins = [
   'http://localhost:5173', 
