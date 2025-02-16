c15a422e775db49ea82ba13816eada5da360069c
SaiSuprith Jukanti <jukantisuprith@gmail.com>
2025-02-16 17:30:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../FrontEnd')));
 
-app.use(cors())
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://ctering-be.vercel.app' 
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed'));
+    }
+  },
+  methods: 'GET,POST,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+};
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 
 const client = require('twilio')(process.env.accountSid, process.env.authToken);
 
