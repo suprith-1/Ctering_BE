1e4ad49a5d598fe5fcd99a655070f147dcea8735
suprith-1 <jukantisuprith@gmail.com>
2025-02-16 16:27:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../FrontEnd')));
+
+app.use(cors())
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const client = require('twilio')(proceenv.accountSid, proceenv.authToken);
+
+const me = 'whatsapp:+919381305670'
+const md = 'whatsapp:+919392646691'
+app.post('/newBooking',(req,res)=>{
+    console.log(req.body);
+    const message = 
+        `*Name:* ${req.body.name}\n\n`
+        + `*Phone:* ${req.body.phone}\n\n`
+        + `*Date:* ${req.body.date}\n\n`
+        + `*Time:* ${req.body.time}\n\n`
+        + `*City:* ${req.body.city}\n\n`
+        + `*Address:* ${req.body.address}\n\n`
+        + `*Type:* ${req.body.type}\n\n`
+        + `*People:* ${req.body.people}\n\n`
+        + `*Items:* ${req.body.items.join(", ")}`;
+    client.messages
+    .create({
+        body: message,
+        from: process.env.From,
+        to: process.env.To,
+    })
+    .then(message => console.log(message.sid))
+    .then(res.json({status:true}));
+})
+
+
+
+const PORT = process.env.PORT 
+app.listen(PORT,()=>{console.log("Server Started")});
